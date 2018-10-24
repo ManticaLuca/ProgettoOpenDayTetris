b2524115bc4452a86afe813567316eb17b418fe2
FerraChris01 <ferrareis.christian@gmail.com>
2018-10-24 17:12:57 +0200

    Update Colore.java

--- src/main/java/com/processing/sketch/Colore.java
+++ src/main/java/com/processing/sketch/Colore.java
@@ -13,20 +13,24 @@ public class Colore {
 
 
     /**
+     * @author Ferrareis Christian
      * colore RED
      */
     private int red;
     /**
+     * @author Ferrareis Christian
      * colore GREEN
      */
     private int green;
     /**
+     * @author Ferrareis Christian
      * colore BLUE
      */
     private int blue;
 
     /**
-     * @brief costruttore vuoto
+     * @author Ferrareis Christian
+     * @brief costruttore vuoto che porta a 0 gli attributi red, green e blue
      */
     public Colore() {
         red = 0;
@@ -35,10 +39,11 @@ public class Colore {
     }
 
     /**
+     * @author Ferrareis Christian
      * @param r R
      * @param g G
      * @param b B
-     * @brief costruttore della classe Colore
+     * @brief costruttore della classe Colore, assegna i valori dati agli attributi red, green e blue 
      */
     public Colore(int r, int g, int b) {
         red = r;
@@ -47,8 +52,9 @@ public class Colore {
     }
 
     /**
+     * @author Ferrareis Christian
      * @param c colore da copiare
-     * @brief effettua una copia del colore dato
+     * @brief effettua una copia del colore passato, va quindi a ad assegnare agli attributi red, green e blue i valori rgb del Colore passato
      */
     public Colore(Colore c) {
         red = c.getR();
@@ -57,6 +63,7 @@ public class Colore {
     }
 
     /**
+     * @author Ferrareis Christian
      * @return r il valore di R
      * @brief restituisce il valore di R
      */
@@ -65,6 +72,7 @@ public class Colore {
     }
 
     /**
+     * @author Ferrareis Christian
      * @return r il valore di G
      * @brief restituisce il valore di G
      */
@@ -73,6 +81,7 @@ public class Colore {
     }
 
     /**
+     * @author Ferrareis Christian
      * @return r il valore di B
      * @brief restituisce il valore di B
      */
@@ -81,7 +90,8 @@ public class Colore {
     }
 
     /**
-     * @brief imposta il colore in base ai parametri
+     * @author Ferrareis Christian
+     * @brief imposta il colore in base ai parametri, va quindi a modificare gli attributi
      * @param red rosso
      * @param green verde
      * @param blue blu
