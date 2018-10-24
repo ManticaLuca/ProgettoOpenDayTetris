8ea0b0fc75e7ae4a64029d9ed1c4bf0adff14a42
Mantica Luca <43844495+ManticaLuca@users.noreply.github.com>
2018-10-24 16:05:55 +0200

    Update gameConstants.java

--- src/main/java/com/processing/sketch/gameConstants.java
+++ src/main/java/com/processing/sketch/gameConstants.java
@@ -1,15 +1,38 @@
 package com.processing.sketch;
 
+/**
+* @author Mantica Luca
+* @brief la classe contiene le costanti di gioco
+*/
 public class gameConstants {
-
+    
+    /**
+     * @author Mantica Luca
+     * @brief altezza della finistra
+     */
     public static final int height=500;
+    
+    /**
+     * @author Mantica Luca
+     * @brief larghezza della finistra
+     */
     public static final int width=500;
 
     /**
-     * grandezza di ogni quadrato
+     * @author Mantica Luca
+     * @brief grandezza di ogni quadrato
      */
     public static final int grandezzaQuadrato= height / 20;
-
+    
+    /**
+     * @author Mantica Luca
+     * @brief velocità di discesa delle forme
+     */
     public static final int velocitaDiscesa=500;
+    
+    /**
+     * @author Mantica Luca
+     * @brief numero di mosse massime per riga
+     */
     public static final int mossePerTurno=5;
 }
