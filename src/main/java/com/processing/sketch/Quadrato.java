c95e3ab68fc07386f3e6d98f6556cde4b37165bb
FerraChris01 <ferrareis.christian@gmail.com>
2018-10-24 18:12:19 +0200

    Update Quadrato.java

--- src/main/java/com/processing/sketch/Quadrato.java
+++ src/main/java/com/processing/sketch/Quadrato.java
@@ -8,17 +8,20 @@ package com.processing.sketch;
 
 public class Quadrato {
     /**
+    * @author Ferrareis Christian
      * numero che rappresenta la coordinata x della posizione del quadrato sul piano
      */
     private int posizioneX;
 
     /**
+     * @author Ferrareis Christian
      * numero che rappresenta la coordinata y della posizione del quadrato sul piano
      */
     private int posizioneY;
 
     /**
-     * @brief costruttore vuoto che setta le coordinate a 0
+     * @author Ferrareis Christian
+     * @brief costruttore vuoto che setta le coordinate {@code posizioneX} e {@code posizioneY} a 0
      */
     public Quadrato() {
         posizioneX = 0;
@@ -26,8 +29,9 @@ public class Quadrato {
     }
 
     /**
+     * @author Ferrareis Christian
      * @param quad oggetto quadrato
-     * @brief metodo che copia le coordinate x e y del quadrato che gli viene passato come parametro
+     * @brief metodo che copia le coordinate {@code posizioneX} e {@code posizioneY} del quadrato che gli viene passato come parametro
      */
     public Quadrato(Quadrato quad) {
         posizioneX = quad.getPosizioneX();
@@ -35,39 +39,44 @@ public class Quadrato {
     }
 
     /**
+     * @author Ferrareis Christian
      * @param posX numero che rappresenta la coordinata x
-     * @brief metodo che permette di modificare la coordinata x del quadrato sul piano
+     * @brief metodo che permette di modificare la coordinata {@code posizioneX} del quadrato sul piano
      */
     public void setPosizioneX(int posX) {
         posizioneX = posX;
     }
 
     /**
+     * @author Ferrareis Christian
      * @param posY numero che rappresenta la coordinata y
-     * @brief metodo che permette di modificare la coordinata y del quadrato sul piano
+     * @brief metodo che permette di modificare la coordinata {@code posizioneY} del quadrato sul piano
      */
     public void setPosizioneY(int posY) {
         posizioneY = posY;
     }
 
     /**
+     * @author Ferrareis Christian
      * @return posizioneX numero che rappresenta la coordinata x
-     * @brief metodo che restituisce la coordinata x della posizione del quadrato sul piano
+     * @brief metodo che restituisce la coordinata {@code posizioneX} della posizione del quadrato sul piano
      */
     public int getPosizioneX() {
         return posizioneX;
     }
 
     /**
+     * @author Ferrareis Christian
      * @return posizioneY numero che rappresenta la coordinata y
-     * @brief metodo che restituisce la coordinata y della posizione del quadrato sul piano
+     * @brief metodo che restituisce la coordinata {@code posizioneY} della posizione del quadrato sul piano
      */
     public int getPosizioneY() {
         return posizioneY;
     }
 
     /**
-     * @brief metodo che restituisce, sottoforma di stringa, le coordinate della posizione del quadrato sul piano
+     * @author Ferrareis Christian
+     * @brief metodo che restituisce, sottoforma di stringa, le coordinate {@code posizioneX}, {@code posizioneY} della posizione del quadrato sul piano
      */
     public String toString() {
         return "pos(" + posizioneX + "," + posizioneY + ")";
