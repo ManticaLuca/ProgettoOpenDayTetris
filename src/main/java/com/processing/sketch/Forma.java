d4e827e870a3e84b630fcd15b01e30c6450acd82
FerraChris01 <ferrareis.christian@gmail.com>
2018-10-24 18:05:11 +0200

    Update Forma.java

--- src/main/java/com/processing/sketch/Forma.java
+++ src/main/java/com/processing/sketch/Forma.java
@@ -52,8 +52,8 @@ public class Forma {
 
     /**
      * @author Ferrareis Christian
-     * @brief costruttore vuoto, che crea una forma casuale, le assegna un colore casuale e infine la attiva. Va ad assegnare dei valori gli attributi forma, verso, 
-       attiva, colore e numElQuadrati e aggiunge al vettore insiemeQuadrati i quadrati che compongono la forma.
+     * @brief costruttore vuoto, che crea una forma casuale, le assegna un colore casuale e infine la attiva. Va ad assegnare dei valori a {@code forma}, {@code verso}, 
+       {@code attiva}, {@code colore} e {@code numElQuadrati} e aggiunge al vettore {@code insiemeQuadrati} i quadrati che compongono la forma.
      */
     public Forma() {
         colore = new Colore();
@@ -75,7 +75,7 @@ public class Forma {
 
     /**
      * @author Ferrareis Christian
-     * @brief imposta il colore (quindi modifica gli attributi red, green, blue della classe colore) della forma tipo: 0 linea azzurra; 1 quadrato giallo; 2 forma "l" arancione; 3 forma "j" blu; 4 forma "z" rosso;
+     * @brief imposta il colore (quindi modifica {@code red}, {@code green} e {@code blue}) della forma tipo: 0 linea azzurra; 1 quadrato giallo; 2 forma "l" arancione; 3 forma "j" blu; 4 forma "z" rosso;
      * 5 forma "s" verde; 6 forma"t" viola.
      */
     public void setColore(){
@@ -107,7 +107,7 @@ public class Forma {
     /**
      * @author Ferrareis Christian
      * @param f oggetto forma che gli viene passato
-     * @brief costruttore che copia negli attributi colore, attiva, verso, forma e numElQuadrati i parametri della forma che gli viene passata e infine ne crea la forma
+     * @brief costruttore che copia in {@code colore}, {@code attiva}, {@code verso}, {@code forma} e {@code numElQuadrati} gli attributi della forma che gli viene passata e infine ne crea la forma
      */
     public Forma(Forma f) {
         colore = new Colore(f.colore);
@@ -132,7 +132,7 @@ public class Forma {
      * @author Ferrareis Christian
      * @param pos posizione del quadrato nel vettore
      * @return oggetto quadrato
-     * @brief metodo che ritorna il quadrato contenuto nel vettore insiemeQuadrati di cui viene passata la posizione
+     * @brief metodo che ritorna il quadrato contenuto nel vettore {@code insiemeQuadrati} di cui viene passata la posizione
      */
     public Quadrato getQuad(int pos) {
         return insiemeQuadrati[pos];
@@ -141,8 +141,8 @@ public class Forma {
     /**
      * @author Ferrareis Christian
      * @param posY coordinata y del quadrato da cancellare
-     * @brief metodo per cancellare un quadrato in caso sia presente una riga completa, decrementa l'attributo numElQuadrati e 
-       cancella un quadrato dal vettore insiemeQuadrati.
+     * @brief metodo per cancellare un quadrato in caso sia presente una riga completa, decrementa {@code numElQuadrati} e 
+       cancella un quadrato dal vettore {@code insiemeQuadrati}.
      */
     public void cancellaQuadrato(int posY) {
         int i = 0;
@@ -160,7 +160,7 @@ public class Forma {
     /**
      * @author Ferrareis Christian
      * @return boolean true se il quadrato è effettivamente presente all'interno del vettore, false se non lo è
-     * @brief metodo che controlla se nel vettore insiemeQuadrati è presente il quadrato di cui viene data la posizione x, y, 
+     * @brief metodo che controlla se nel vettore {@code insiemeQuadrati} è presente il quadrato di cui viene data la posizione x, y, 
      * @par posX coordinata x
      * @par posY coordinata y
      */
@@ -177,7 +177,7 @@ public class Forma {
      * @author Ferrareis Christian
      * @param i posizione del quadrato nel vettore
      * @return posizione coordinate del quadrato sottoforma di stringa
-     * @brief metodo che ritorna, sotto forma di stringa, le coordinate x, y (attributo posizione) del quadrato del vettore insiemeQuadrati di cui viene data la posizione nel vettore
+     * @brief metodo che ritorna, sotto forma di stringa, le coordinate x, y ({@code posX}, {@code posY}) del quadrato del vettore insiemeQuadrati di cui viene data la posizione nel vettore
      */
     public String getPos(int i) {
         String posizione = Integer.toString(insiemeQuadrati[i].getPosizioneX()) + ";" + Integer.toString(insiemeQuadrati[i].getPosizioneY());
@@ -187,7 +187,7 @@ public class Forma {
     /**
      * @author Ferrareis Christian
      * @return attiva stato di attivazione
-     * @brief metodo che ritorna attiva, quindi lo stato di attivazione dell'oggetto forma
+     * @brief metodo che ritorna {@code attiva}, quindi lo stato di attivazione dell'oggetto forma
      */
     public boolean getAttiva() {
         return attiva;
@@ -196,7 +196,7 @@ public class Forma {
     /**
      * @author Ferrareis Christian
      * @return forma numero che rappresenta il tipo di forma
-     * @brief metodo che ritorna forma, quindi il tipo di forma dell'oggetto forma
+     * @brief metodo che ritorna {@code forma}, quindi il tipo di forma dell'oggetto forma
      */
     public int getTipo() {
         return forma;
@@ -205,7 +205,7 @@ public class Forma {
     /**
      * @author Ferrareis Christian
      * @return verso numero che rappresenta il verso dell'oggetto forma
-     * @brief metodo che ritorna verso, quindi il verso di orientamento dell'oggetto forma
+     * @brief metodo che ritorna {@code verso}, quindi il verso di orientamento dell'oggetto forma
      */
     public int getVerso() {
         return verso;
@@ -214,7 +214,7 @@ public class Forma {
     /**
      * @author Ferrareis Christian
      * @return numElQuadrati numero di quadrati
-     * @brief metodo che ritorna numElQuadrati, quindi il numero di quadrati utilizzati dall'oggetto forma
+     * @brief metodo che ritorna {@code numElQuadrati}, quindi il numero di quadrati utilizzati dall'oggetto forma
      */
     public int getNumElQuadrati() {
         return numElQuadrati;
@@ -223,7 +223,7 @@ public class Forma {
     /**
      * @author Ferrareis Christian
      * @return colore colore dell'oggetto forma
-     * @brief metodo che ritorna colore, quindi il colore dell'oggetto forma
+     * @brief metodo che ritorna {@code colore}, quindi il colore dell'oggetto forma
      */
     public Colore getColore() {
         return colore;
@@ -232,7 +232,7 @@ public class Forma {
     /**
      * @author Ferrareis Christian
      * @param posY posizione y dell'oggetto forma
-     * @brief metodo che permette di far arrivare l'oggetto forma sul fondo (modificandone posY), e lo fa incrementando la posizione y di 25, inteso come numero di pixel
+     * @brief metodo che permette di far arrivare l'oggetto forma sul fondo (modificandone {@code posY}), e lo fa incrementando la posizione y di 25, inteso come numero di pixel
      */
     public void incremento(int posY) {
         for (int i = 0; i < numElQuadrati; i++) {
@@ -245,7 +245,7 @@ public class Forma {
     /**
      * @author Ferrareis Christian
      * @param attiv stato true (attiva) o false (disattivaFormaAttiva)
-     * @brief metodo che permette di modificare attiva, quindi lo stato di attivazione l'oggetto forma
+     * @brief metodo che permette di modificare {@code attiva}, quindi lo stato di attivazione l'oggetto forma
      */
     public void setAttiva(boolean attiv) {
         attiva = attiv;
@@ -256,7 +256,7 @@ public class Forma {
      * @param posX     coordinata x
      * @param posY     coordinata y
      * @param quadrato posizione del quadrato nel vettore
-     * @brief metodo che permette di settare la posizione del quadrato (posX e posY) nella zona richiesta tramite coordinate x e y
+     * @brief metodo che permette di settare la posizione del quadrato ({@code posX}, {@code posY}) nella zona richiesta tramite coordinate x e y
      */
     public void setPos(int posX, int posY, int quadrato) {
         insiemeQuadrati[quadrato].setPosizioneX(posX);
@@ -266,7 +266,7 @@ public class Forma {
     /**
      * @author Ferrareis Christian
      * @param verso numero che rappresenta il nuovo verso (sopra, sotto, destra o sinistra)
-     * @brief metodo che permette di modificare verso, quindi il verso di orientamento dell'oggetto forma
+     * @brief metodo che permette di modificare {@code verso}, quindi il verso di orientamento dell'oggetto forma
      */
     public void setVerso(int verso) {
         this.verso = verso;
@@ -275,7 +275,7 @@ public class Forma {
     /**
      * @author Ferrareis Christian
      * @return str posizioni x e y dei quadrati sottoforma di stringa
-     * @brief metodo che ritorna, sottoforma di stringa, posX e posY di ogni quadrato nel vettore
+     * @brief metodo che ritorna, sottoforma di stringa, {@code posX} e {@code posY} di ogni quadrato nel vettore
      */
     public String toString() {
         String str = "";
